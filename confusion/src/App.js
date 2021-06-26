09de5b5f003853d7dbb882c3c245d3d5cff03e92
valentin-vg <valentin.gavela@despegar.com>
2021-06-26 18:15:35 -0300

    React Router

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,12 +1,15 @@
-import { Component } from "react";
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import { Component } from "react";
 import { Main } from "./components/MainComponent";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
