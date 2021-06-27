9275e522f114dd9c4eab32e9bf8f28629cef1e9f
valentin-vg <valentin.gavela@despegar.com>
2021-06-27 19:26:18 -0300

    Single Page Applications Part 1

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,29 +1,41 @@
 import React, { Component } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { Menu } from "./MenuComponent";
-import { DISHES } from "../shared/dishes";
 import { Header } from "./HeaderComponent";
 import { Footer } from "./FooterComponent";
 import { Home } from "./HomeComponent";
+import { Contact } from "./ContactComponent";
+
+import { COMMENTS } from "../shared/comments";
+import { DISHES } from "../shared/dishes";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
   render() {
-    const HomePage = () => {
-      return <Home />;
-    };
+    const HomePage = () => (
+      <Home
+        dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+        promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+        leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+      />
+    );
 
     return (
       <div>
         <Header />
         <Switch>
+          <Route exact path="/contactus" component={Contact} />
           <Route path="/home" component={HomePage} />
           <Route
             exact
